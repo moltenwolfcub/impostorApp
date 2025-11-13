1f278cc13e088c4e7667e296c03c8a591ba85bf7
MoltenWolfCub <oliver@tomlins.org.uk>
2025-11-13 21:45:33 +0000

    Upgraded activity_game.xml
    still not the final version but now it has the major funcionality details

--- app/src/main/java/com/moltenwolfcub/impostor/GameActivity.java
+++ app/src/main/java/com/moltenwolfcub/impostor/GameActivity.java
@@ -1,7 +1,11 @@
 package com.moltenwolfcub.impostor;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
 import android.widget.Button;
+import android.widget.FrameLayout;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
@@ -10,21 +14,28 @@ import androidx.appcompat.app.AppCompatActivity;
 public class GameActivity extends AppCompatActivity {
     public Game game;
 
-    private TextView playerName;
+    private TextView playerNameFront;
+    private TextView playerNameBack;
     private Button nextButton;
+    private TextView secretWordDisplay;
 
-    private TextView imposterLabel;
-    private TextView wordDisplay;
+    private FrameLayout cardContainer;
+    private View frontCard;
+    private View backCard;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
 
-        playerName = findViewById(R.id.playerName);
+        playerNameFront = findViewById(R.id.playerNameFront);
+        playerNameBack = findViewById(R.id.playerNameBack);
         nextButton = findViewById(R.id.nextButton);
-        imposterLabel = findViewById(R.id.imposterLabel);
-        wordDisplay = findViewById(R.id.wordDisplay);
+        secretWordDisplay = findViewById(R.id.secretWord);
+
+        cardContainer = findViewById(R.id.cardContainer);
+        frontCard = findViewById(R.id.frontCard);
+        backCard = findViewById(R.id.backCard);
 
         game = getIntent().getParcelableExtra("game");
         
@@ -33,24 +44,40 @@ public class GameActivity extends AppCompatActivity {
         nextButton.setOnClickListener(view -> {
             if (game.hasNextPlayer()) {
                 game.nextPlayer();
+                backCard.setVisibility(View.GONE);
+                frontCard.setVisibility(View.VISIBLE);
                 updateUI();
             } else {
                 //TODO names given out
                 finish();
             }
         });
+
+        cardContainer.setOnTouchListener((view, event) -> {
+            switch (event.getAction()) {
+                case MotionEvent.ACTION_DOWN:
+                    frontCard.setVisibility(View.GONE);
+                    backCard.setVisibility(View.VISIBLE);
+                    return true;
+                case MotionEvent.ACTION_UP:
+                case MotionEvent.ACTION_CANCEL:
+                    backCard.setVisibility(View.GONE);
+                    frontCard.setVisibility(View.VISIBLE);
+                    return true;
+            }
+            return false;
+        });
     }
 
     private void updateUI() {
         Player current = game.getCurrentPlayer();
-        playerName.setText(current.name.getName());
+        playerNameFront.setText(current.name.getName());
+        playerNameBack.setText(current.name.getName());
 
         if (current.isImposter) {
-            imposterLabel.setText("Imposter");
-            wordDisplay.setText("You don't get to know the word");
+            secretWordDisplay.setText(R.string.text_imposterInfo);
         } else {
-            imposterLabel.setText("Not imposter");
-            wordDisplay.setText(game.secretWord);
+            secretWordDisplay.setText(game.secretWord);
         }
     }
 }
